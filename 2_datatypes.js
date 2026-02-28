49b0750839ca75189174b2c0ed5db8bedc6335ac
SuhamPandey <pandeysuham280@gmail.com>
2026-02-28 21:23:41 +0545

    Datatypes

---
+++ 2_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";      // treat all js code as newer version
+
+// alert(3 + 3)    //we are using nodejs, not browser 
+console.log(3+3)
+console.log("suham");
+
+
+let name = "suham"          // string
+let age = 22                // number
+let isLoggedIn = false      //false
+let state;                  //undefined
+
+
+//number => 2 to power 53
+//BigInt
+//string => ""
+//boolean => true or false
+//null => standalone value 
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "suham");
+console.log(typeof age);
+console.log(typeof null);               //object
+console.log(typeof undefined);          //undefined
+
+
+
