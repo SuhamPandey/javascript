513ae705b309992313be56ad549dcdd7bcbf4ccc
SuhamPandey <pandeysuham280@gmail.com>
2026-02-28 21:24:24 +0545

    Conversion Operation

---
+++ 3_ConversionOperation.js
@@ -0,0 +1,76 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber =  Number(score)               //Number funtion converts string into numeric value 
+//console.log(typeof valueInNumber);
+
+
+let sc = null
+//console.log(typeof sc);
+//console.log(typeof (sc));
+let valueInNumbe =  Number(sc)   
+//console.log(valueInNumbe);
+
+
+// "33" => 33
+//"33abc"  => NaN 
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false
+// "suham" => true
+
+
+let someNumber = 33 
+let stringnumber = String(someNumber)
+//console.log(stringnumber);
+//console.log(typeof stringnumber);
+
+
+
+// *********************Operations**********************
+
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " suham"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100 
+++gamecounter
+console.log(gamecounter)
+
+
