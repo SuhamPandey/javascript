a67a41189b8bb7e19791b0653e03fbf7cbf60f74
SuhamPandey <pandeysuham280@gmail.com>
2026-05-17 00:44:51 +0545

    Add files via upload

---
+++ 4_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1 );
+// console.log(2 < 1 );
+// console.log(2 == 1 );            
+// console.log(2 != 1 );
+
+
+// console.log("2" > 1);                      
+// console.log("02" > 1);
+
+// console.log(null > 0 );             
+// console.log(null == 0 );
+// console.log(null >= 0 );
+
+// equality check == and comparisions like < > <= >= works differently
+// comparision converts null to a number, treating it as 0
+// thats why null >= 0 is true
+
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+console.log("2" === 2 );
+
